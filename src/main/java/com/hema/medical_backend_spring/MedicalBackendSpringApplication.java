590c81171f105400b0f603aed15fa5beebac1c86
hima <ebrahumahmed2003@gmail.com>
2026-03-02 02:06:50 +0200

    getting strated with spring boot

---
+++ src/main/java/com/hema/medical_backend_spring/MedicalBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hema.medical_backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hema/medical_backend_spring/MedicalBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hema.medical_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
