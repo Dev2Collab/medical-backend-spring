b3783096cd99a6d7266a868d3f359bd409804626
hima <ebrahumahmed2003@gmail.com>
2026-03-13 18:48:49 +0200

    Add user details service and password encoder to security configurationm

--- src/main/java/com/hema/medical_backend_spring/config/ProjectSecurityConfig.java
+++ src/main/java/com/hema/medical_backend_spring/config/ProjectSecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,4 +21,18 @@ public class ProjectSecurityConfig {
         .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean 
+    UserDetailsService userDetailsService(){
+        UserDetails user1=User.withUsername("hema").password("{noop}12345").authorities("read").build();
+        UserDetails user2=User.withUsername("admin").password("{noop}12345").authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    @Bean 
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
